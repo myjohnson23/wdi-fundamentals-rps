71a2919eb095dcde4a60b9dceee6b829a71d18c1
Myron Johnson <myronjohnson@Myrons-MacBook-Pro.local>
2015-06-27 16:03:10 -0500

    Added function for playing to five

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -55,7 +55,19 @@ function playToFive() {
     var playerWins = 0;
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+    while ((playerWins !==5) && (computerWins !==5)) {
+        var playerMove = getPlayerMove();
+        var computerMove = getComputerMove();
+        var winner = getWinner(playerMove,computerMove);
+        if (winner === 'player') {
+            playerWins = playerWins + 1;
+        }
+        else if (winner === 'computer') {
+            computerWins = computerWins + 1;
+        }
+        console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+        console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+    }
     return [playerWins, computerWins];
 }
 
